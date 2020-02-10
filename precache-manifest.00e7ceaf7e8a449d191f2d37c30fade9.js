af8ec56b64de09b8aa7551ba08bd7ca63ed0e1d9
Farbod Faramarzi <fabbe@suprawsm.com>
2020-02-10 21:06:29 +0100

    first commit

---
+++ precache-manifest.00e7ceaf7e8a449d191f2d37c30fade9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7e85d7c4eb8e5e5c0f61d1995dfa637",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d5b1994764624a6c150b",
+    "url": "./static/css/main.a53260cf.chunk.css"
+  },
+  {
+    "revision": "7c127988b70e94a9eaa0",
+    "url": "./static/js/2.eb36b0c3.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "./static/js/2.eb36b0c3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d5b1994764624a6c150b",
+    "url": "./static/js/main.f1c7bf98.chunk.js"
+  },
+  {
+    "revision": "69d2edf18ff43aab7317",
+    "url": "./static/js/runtime-main.6ce1bc07.js"
+  },
+  {
+    "revision": "029fdc07e0008d64873c1d4730fe4960",
+    "url": "./static/media/logo.029fdc07.svg"
+  }
+]);
